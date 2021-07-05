0264594046923e232d05755584f4797fdcdeb373
sammykirigha <dkirigha18@gmail.com>
2021-07-05 18:13:55 +0300

    added something new

--- selection.js
+++ selection.js
@@ -19,3 +19,18 @@ function selectionSort(arr) {
 }
 
 console.log(selectionSort([4, 8, 9, 1, 3, 2, 5, 7, 6, 10]))
+
+
+//find two elements in array that add up to a given number
+function sum(arr, num) {
+    for (let i = 0; i < arr.length; i++){
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[i] + arr[j] == num) {
+                return [arr[i],arr[j]]
+            }
+        }
+    }
+    return false
+}
+
+console.log(sum([2,7,4,6,8], 9))
